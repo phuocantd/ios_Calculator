1a6ad9315ae9676911e85bf1b6e79694bbb33c9f
phuocantd <phuocantd@gmail.com>
2018-11-08 17:52:36 +0700

    upload button simple calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,121 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberOfScreen:Double = 0;
+    var previousNumbers:Double = 0;
+    var performingMath:Bool = true;
+    var operators:Int = 0
+    
+    @IBOutlet weak var screen: UILabel!
+    @IBOutlet weak var preScreen: UILabel!
+    
+    func displayPreScreen(){
+        if operators >= 14 && operators <= 17{
+            preScreen.text = String(previousNumbers)
+            switch operators{
+            case 14:
+                preScreen.text = preScreen.text! + "/"
+                break
+            case 15:
+                preScreen.text = preScreen.text! + "x"
+                break
+            case 16:
+                preScreen.text = preScreen.text! + "-"
+                break
+            case 17:
+                preScreen.text = preScreen.text! + "+"
+                break
+            default:
+                break
+            }
+        }
+    }
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        displayPreScreen()
+        if screen.text == "0"{
+            performingMath = true
+        }
+        if performingMath == true{
+            screen.text = String(sender.tag)
+            numberOfScreen = Double(screen.text!)!
+            performingMath = false
+        }
+        else{
+            screen.text = screen.text! + String(sender.tag)
+            numberOfScreen = Double(screen.text!)!
+        }
+        
+        
+    }
+    
+    func calc(){
+        switch operators{
+        case 14:
+            numberOfScreen = previousNumbers/numberOfScreen
+            break
+        case 15:
+            numberOfScreen = previousNumbers*numberOfScreen
+            break
+        case 16:
+            numberOfScreen = previousNumbers-numberOfScreen
+            break
+        case 17:
+            numberOfScreen = previousNumbers+numberOfScreen
+            break
+        default:
+            
+            break
+        }
+        //previousNumbers = 0
+        
+    }
+    
+    @IBAction func operation(_ sender: UIButton) {
+        if sender.tag >= 14 && sender.tag <= 17{
+            
+            performingMath = true
+            calc()
+            operators = sender.tag
+            previousNumbers = numberOfScreen
+            numberOfScreen = 0
+            switch sender.tag{
+            case 14:    //divide        -- button [/]
+                screen.text = "/"
+                break
+            case 15:    //multiply      -- button [x]
+                screen.text = "x"
+                break
+            case 16:    //minus         -- button [-]
+                screen.text = "-"
+                break
+            case 17:    //plus          -- button [+]
+                screen.text = "+"
+                break
+            default:
+                break
+            }
+            displayPreScreen()
+        }
+        else if sender.tag == 18{   //equal         -- button [=]
+            preScreen.text = ""
+            performingMath = true
+            calc()
+            screen.text = String(numberOfScreen)
+            previousNumbers = 0
+            operators = 0
+        }
+        else if sender.tag == 12{   //clear -- button [C]
+            preScreen.text = ""
+            numberOfScreen = 0
+            previousNumbers = 0
+            operators = 0
+            performingMath = false
+            screen.text = "0"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
